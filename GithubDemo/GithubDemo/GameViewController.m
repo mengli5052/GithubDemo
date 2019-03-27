6679e846b0b7928064768eaba68932f3febcbbac
megnli5052 <1451051606@qq.com>
2019-03-27 10:49:27 +0800

    createGameViewController

---
+++ GithubDemo/GithubDemo/GameViewController.m
@@ -0,0 +1,34 @@
+//
+//  GameViewController.m
+//  GithubDemo
+//
+//  Created by 众忆捷科技 on 2019/3/27.
+//  Copyright © 2019 众忆捷科技. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+    NSLog(@"This is the game!");
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
