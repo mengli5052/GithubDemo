c248ec6ba921cc6cf58877fff4e67897ac61d679
megnli5052 <1451051606@qq.com>
2019-03-27 10:53:55 +0800

    create singer view controller

---
+++ GithubDemo/GithubDemo/SingerViewController.m
@@ -0,0 +1,34 @@
+//
+//  SingerViewController.m
+//  GithubDemo
+//
+//  Created by 众忆捷科技 on 2019/3/27.
+//  Copyright © 2019 众忆捷科技. All rights reserved.
+//
+
+#import "SingerViewController.h"
+
+@interface SingerViewController ()
+
+@end
+
+@implementation SingerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor greenColor];
+    NSLog(@"This is  a singer!");
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
